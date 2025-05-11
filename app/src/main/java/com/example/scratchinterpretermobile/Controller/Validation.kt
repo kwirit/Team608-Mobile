cd984242771d1984367b132d9b2eb75b0080ae59
kwirit <bartsimpsons044@gmail.com>
2025-05-12 00:38:47 +0700

    add: basis for data validation and expression calculation has been created add: basic test for validation var, transferPrefixToPostfix

--- app/src/main/java/com/example/scratchinterpretermobile/Controller/Validation.kt
+++ app/src/main/java/com/example/scratchinterpretermobile/Controller/Validation.kt
@@ -4,6 +4,8 @@ import com.example.scratchinterpretermobile.Controller.Error.Error
 import com.example.scratchinterpretermobile.Controller.Error.ErrorStore
 import com.example.scratchinterpretermobile.Model.Containers
 import com.example.scratchinterpretermobile.Model.Stack
+import com.example.scratchinterpretermobile.Model.Containers.context
+import com.example.scratchinterpretermobile.Model.VarBlock
 
 fun validateNameVariable(input: String): Int {
     val nameVariable = input.trim() // Удаление табуляции справа и слева
@@ -30,7 +32,6 @@ fun validateNameVariable(input: String): Int {
 }
 
 
-
 fun getElementFromString(input: String): MutableList<String> {
     val trimmedInput = input.trim()
 
@@ -38,25 +39,22 @@ fun getElementFromString(input: String): MutableList<String> {
     val operators = "+-*%/()"
 
     var currentToken = StringBuilder()
-    var isVar = false;
 
     for (symbol in trimmedInput) {
         if (symbol == ' ') continue
 
         if (currentToken.isNotEmpty() && (symbol.isDigit() || symbol.isLetter() || symbol == '_')) {
             currentToken.append(symbol)
-        } else if (currentToken.isEmpty() && (symbol.isDigit() || symbol.isLetter() || symbol == '_')){
+        } else if (currentToken.isEmpty() && (symbol.isDigit() || symbol.isLetter() || symbol == '_')) {
             currentToken = StringBuilder().append(symbol)
-        }
-
-        else if (symbol in operators) {
+        } else if (symbol in operators) {
             if (currentToken.isNotEmpty()) {
                 elements.add(currentToken.toString())
                 currentToken.clear()
             }
             elements.add(symbol.toString())
         } else {
-            currentToken.append(symbol);
+            currentToken.append(symbol)
         }
     }
 
@@ -65,5 +63,107 @@ fun getElementFromString(input: String): MutableList<String> {
     }
 
     return elements
+}
+
+
+fun transferPrefixToPostfix(elements: MutableList<String>): MutableList<String> {
+    var postfix = mutableListOf<String>()
+    var stack = Stack<String>()
+    var priority = mapOf(
+        "%" to 2,
+        "/" to 2,
+        "*" to 2,
+        "+" to 1,
+        "-" to 1,
+        "(" to 0
+    )
+
+    val operators = "+-*%/"
+
+    for (element in elements) {
+        when {
+            element != "(" && element !in operators && element != ")" -> {
+                postfix.add(element)
+            }
+
+            element == "(" -> {
+                stack.push(element)
+            }
+
+            element == ")" -> {
+                while (!stack.isEmpty() && stack.peek() != "(") {
+                    postfix.add(stack.pop()!!)
+                }
+                if (!stack.isEmpty()) stack.pop()
+            }
+
+            element in operators -> {
+                while (!stack.isEmpty()
+                    && stack.peek() != "("
+                    && priority[element]!! <= priority[stack.peek()]!!
+                ) {
+                    postfix.add(stack.pop()!!)
+                }
+                stack.push(element)
+            }
+        }
+    }
+    while (!stack.isEmpty()) {
+        postfix.add(stack.pop()!!)
+    }
+
+    return postfix
+}
+
+fun findVariableInNameSpace(name: String): Int? {
+    for (namespace: HashMap<String, VarBlock> in context) {
+        if (namespace.containsKey(name)) {
+            return (namespace[name]?.value ?: -1) as Int?;
+        }
+    }
+    return -1
+}
+
+fun calculationPrefix(postfix: MutableList<String>): Int {
+    var stack = Stack<Int>()
+
+    for (element in postfix) {
+        if (element in "+-*%/") {
+            if (stack.size() >= 2) {
+                var first = stack.pop()!!
+                var second = stack.pop()!!
+                when {
+                    element == "-" -> {
+                        stack.push(second - first)
+                    }
+
+                    element == "+" -> {
+                        stack.push(second + first)
+                    }
+
+                    element == "*" -> {
+                        stack.push(second * first)
+                    }
+
+                    element == "%" -> {
+                        stack.push(second % first)
+                    }
+
+                    element == "/" -> {
+                        stack.push(second / first)
+                    }
+                }
+                continue
+            } else
+                return -1
+        }
+
+        var value = element.toIntOrNull()
+        if (value == null) {
+            value = findVariableInNameSpace(element)
+        }
+        stack.push(value!!)
+    }
+    return stack.pop()!!;
 }
 
--- app/src/test/java/getElementFromStringUnitTest.kt
+++ app/src/test/java/getElementFromStringUnitTest.kt
@@ -4,9 +4,18 @@ import com.example.scratchinterpretermobile.Controller.getElementFromString
 
 class getElementFromStringUnitTest {
     @Test
-    fun validName_returnsSuccess() {
+    fun test() {
         val result = getElementFromString("a + 232 - 12+b * (5 % 10 (2 + 1) / ad)");
         assertEquals(listOf("a", "+", "232", "-", "12", "+", "b", "*", "(", "5", "%", "10", "(", "2", "+", "1", ")", "/", "ad", ")"), result)
     }
-
+    @Test
+    fun validName_returnsSuccess() {
+        val result = getElementFromString("a+232-12+b * (5 % 10 (2 + 1) / ad3131)");
+        assertEquals(listOf("a", "+", "232", "-", "12", "+", "b", "*", "(", "5", "%", "10", "(", "2", "+", "1", ")", "/", "ad3131", ")"), result)
+    }
+    @Test
+    fun array() {
+        val result = getElementFromString("a[b]+232-12+b * (5 % 10 (2 + 1) / ad3131)");
+        assertEquals(listOf("a[b]", "+", "232", "-", "12", "+", "b", "*", "(", "5", "%", "10", "(", "2", "+", "1", ")", "/", "ad3131", ")"), result)
+    }
 }
