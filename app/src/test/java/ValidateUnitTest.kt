a91290b7b430d7c1d0ab970c043d51d443aad4df
kwirit <bartsimpsons044@gmail.com>
2025-05-11 22:19:31 +0700

    update: return validateNameVariable

--- app/src/test/java/ValidateUnitTest.kt
+++ app/src/test/java/ValidateUnitTest.kt
@@ -6,36 +6,36 @@ class ValidateUnitTest {
     @Test
     fun validName_returnsSuccess() {
         val error = validateNameVariable("a123");
-        assertEquals(0, error?.id ?: -1)
+        assertEquals(0, error)
     }
 
     @Test
     fun startsWithDigit_returnsError101() {
         val error = validateNameVariable("123");
-        assertEquals(101, error?.id ?: -1)
+        assertEquals(101, error)
     }
 
     @Test
     fun startByUnderscores_returnsSuccess() {
         val error = validateNameVariable("_123");
-        assertEquals(0, error?.id ?: -1)
+        assertEquals(0, error)
     }
 
     @Test
     fun invalidCharacters_returnsError103() {
         val error = validateNameVariable("a-b");
-        assertEquals(103, error?.id ?: -1)
+        assertEquals(103, error)
     }
 
     @Test
     fun containsSpace_returnsError102() {
         val error = validateNameVariable("mew mew mew");
-        assertEquals(102, error?.id ?: -1)
+        assertEquals(102, error)
     }
 
     @Test
     fun emptyInput_returnsError104() {
         val error = validateNameVariable("   ")
-        assertEquals(104, error?.id ?: -1)
+        assertEquals(104, error)
     }
 }
