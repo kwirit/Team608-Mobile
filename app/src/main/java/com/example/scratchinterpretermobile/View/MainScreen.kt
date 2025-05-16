c863de8d8f9f729dcb56bead74a978c662ccac29
Egor_Granin <epichescktdm@gmail.com>
2025-05-16 20:14:59 +0700

    added smooth drag and drop

--- app/src/main/java/com/example/scratchinterpretermobile/View/MainScreen.kt
+++ app/src/main/java/com/example/scratchinterpretermobile/View/MainScreen.kt
@@ -1,31 +1,21 @@
 package com.example.scratchinterpretermobile.View
+import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.material3.Surface
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import com.example.scratchinterpretermobile.R
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
@@ -35,38 +25,44 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.input.pointer.pointerInput
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Dialog
 import androidx.compose.ui.window.DialogProperties
 import com.example.scratchinterpretermobile.Model.Variable
-import com.example.scratchinterpretermobile.View.DraggedBlock.Initialization
 import com.example.scratchinterpretermobile.ui.theme.Blue
-import com.example.scratchinterpretermobile.ui.theme.LightOrange
-import com.example.scratchinterpretermobile.ui.theme.Orange
+import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.detectDragGestures
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.unit.dp
+import org.burnoutcrew.reorderable.*
 
 @Composable
 fun MainScreen(viewModel: MainViewModel){
     val showBoxesState = remember { mutableStateOf(false) }
 
-    var listOfBoxes = remember { mutableStateListOf<ProgramBox>() }
+    val listOfBoxes = remember { mutableStateListOf<ProgramBox>() }
 
     if(showBoxesState.value == true){
         ShowListOfBoxes(showBoxesState, viewModel, listOfBoxes)
     }
+
     Column {
         TopBar(showBoxesState)
-        Column ( Modifier.weight(1f).verticalScroll(rememberScrollState())){
-            listOfBoxes.forEach { item->
-                item.render()
-            }
-        }
+//        Column ( Modifier.weight(1f).verticalScroll(rememberScrollState())){
+//            listOfBoxes.forEach { item->
+//                item.render()
+//            }
+//        }
+        VerticalReorderList(listOfBoxes)
         BottomBar()
     }
 }
@@ -121,13 +117,44 @@ fun ListOfVar(variables: MutableList<Variable>){
 
 @Composable
 fun BaseBox(name: String,content:@Composable () -> Unit){
-    Card(Modifier.fillMaxWidth().height(200.dp).padding(10.dp),
+    Card(Modifier.fillMaxWidth().height(100.dp).padding(10.dp),
         colors = CardDefaults.cardColors(containerColor = Blue),
     ) {
         Box(Modifier.fillMaxWidth(),contentAlignment = Alignment.Center){
             Column {Text(text = name, textAlign = TextAlign.Center)
                 Row(Modifier.padding(10.dp)){
                     content()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun VerticalReorderList(list: MutableList<ProgramBox>) {
+    val data by rememberUpdatedState(newValue = list)
+    val state = rememberReorderableLazyListState(onMove = { from, to ->
+        data.apply {
+            add(to.index, removeAt(from.index))
+        }
+    })
+
+    LazyColumn(
+        state = state.listState,
+        modifier = Modifier
+            .reorderable(state)
+            .detectReorderAfterLongPress(state)
+            .fillMaxSize()
+    ) {
+        items(data, key = { it.id }) { item ->
+            ReorderableItem(state, key = item) { isDragging ->
+                val elevation = animateDpAsState(if (isDragging) 30.dp else 0.dp)
+                Column(
+                    modifier = Modifier
+                        .shadow(elevation.value)
+                        .fillMaxWidth()
+                ) {
+                    item.render()
                 }
             }
         }
