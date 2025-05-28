0469d6c3bff8300e0b6893db8d314bfad2e1da83
Maxim Staricov <Max47911.eng@gmail.com>
2025-05-28 22:34:16 +0700

    Added: new crutches

--- app/src/main/java/com/example/scratchinterpretermobile/Model/test.kt
+++ app/src/main/java/com/example/scratchinterpretermobile/Model/test.kt
@@ -3,68 +3,30 @@ package com.example.scratchinterpretermobile.Model
 fun main() {
     val scope = UIContext.peekScope()
 
+    val initBlock = InitBlock()
+    initBlock.initIntegerBlock("var")
 
-    val initBlock1 = InitBlock()
-    initBlock1.initIntegerArrayBlock("ar", "5")
 
-    val initBlock2 = InitBlock()
-    initBlock2.initIntegerBlock("i")
-
-    val assignmentBlock1 = AssignmentBlock()
-    val assignmentBlock2 = AssignmentBlock()
+    //-------------------
     val loopBlock = LoopBlock()
+        //-------------------
+        val conditionBlock = ConditionBlock()
+        val assingnment1Block = AssignmentBlock()
+        assingnment1Block.assignIntegerBlock("var", "99")
 
-    assignmentBlock1.assignElementIntegerArrayBlock("ar","i","i")
-    assignmentBlock2.assignIntegerBlock("i", "i + 1")
+        val listBlocksToConditions = mutableListOf<InstructionBlock>()
+        listBlocksToConditions.add(assingnment1Block)
+        conditionBlock.processInput("var","3","==",listBlocksToConditions, mutableListOf())
+        //-------------------
+    val assignmentBlock = AssignmentBlock()
+    assignmentBlock.assignIntegerBlock("var", "var+1")
 
-    val mutableList = mutableListOf<InstructionBlock>()
-    mutableList.add(assignmentBlock1)
-    mutableList.add(assignmentBlock2)
+    val listBlocksToLoop = mutableListOf<InstructionBlock>()
+    listBlocksToLoop.add(conditionBlock)
+    listBlocksToLoop.add(assignmentBlock)
+    loopBlock.processInput("var","10","<", listBlocksToLoop)
+    //-------------------
 
-    loopBlock.processInput("i", "5", "<", mutableList)
-
-
-
-
-
-//    val initBlock1 = InitBlock()
-//    initBlock1.initIntegerArrayBlock("ar", "5")
-//
-//    val assignmentBlock1 = AssignmentBlock()
-//    assignmentBlock1.assignIntegerArrayBlock("ar", "5,4,3,2,1")
-//
-//    val initBlock2 = InitBlock()
-//    initBlock2.initIntegerBlock("i")
-//
-//    val loopBlock1 = LoopBlock()
-//    // -------------------------
-//    val initBlock3 = InitBlock()
-//    initBlock3.initIntegerBlock("j")
-//    val loopBlock2 = LoopBlock()
-//        //--------------------------
-//        val conditionBlock = ConditionBlock()
-//            //--------------------------
-//            val assignmentBlock2 = AssignmentBlock()
-//            assignmentBlock2.assignElementIntegerArrayBlock("ar","j","ar[i]")
-//
-//            val conditionBlocks = mutableListOf<InstructionBlock>()
-//            conditionBlocks.add(assignmentBlock2)
-//            //--------------------------
-//        val assignmentBlock3 = AssignmentBlock()
-//        assignmentBlock3.assignIntegerBlock("j","j+1")
-//
-//        val loop2Blocks = mutableListOf<InstructionBlock>()
-//        loop2Blocks.add(conditionBlock)
-//        loop2Blocks.add(assignmentBlock3)
-//        //--------------------------
-//    val assignmentBlock4 = AssignmentBlock()
-//    assignmentBlock4.assignIntegerBlock("i", "i+1")
-//
-//    val loop1Blocks = mutableListOf<InstructionBlock>()
-//    loop1Blocks.add(initBlock3)
-//    loop1Blocks.add(loopBlock2)
-//    loop1Blocks.add(assignmentBlock4)
-//    //--------------------------
-//    loopBlock2.processInput("j","5","<",loop2Blocks)
-//    loopBlock1.processInput("i","5","<",loop1Blocks)
+    loopBlock.removeBlock()
+    val error = loopBlock.run()
 }
