9ca2745776f1044fc8bda011d5b6f89f6f229d16
kwirit <bartsimpsons044@gmail.com>
2025-05-29 13:26:43 +0700

    add: split scope in ConditionBlock; addScopeToContext in LoopBlock and ConditionBlock; newError about NO_COMPARISON_OPERATOR_SELECTED

--- app/src/main/java/com/example/scratchinterpretermobile/Model/ConditionBlock.kt
+++ app/src/main/java/com/example/scratchinterpretermobile/Model/ConditionBlock.kt
@@ -1,5 +1,6 @@
 package com.example.scratchinterpretermobile.Model
 
+import com.example.scratchinterpretermobile.Controller.Error.NO_COMPARISON_OPERATOR_SELECTED
 import com.example.scratchinterpretermobile.Controller.Error.SUCCESS
 import com.example.scratchinterpretermobile.Controller.calculationArithmeticExpression
 
@@ -16,12 +17,50 @@ class ConditionBlock(
     private var thenBlock: MutableList<InstructionBlock> = mutableListOf()
     private var elseBlock: MutableList<InstructionBlock> = mutableListOf()
 
-    private var scope: HashMap<String, VarBlock<*>> = hashMapOf();
+    private var elseScope: HashMap<String, VarBlock<*>> = hashMapOf()
+    private var thenScope: HashMap<String, VarBlock<*>> = hashMapOf()
 
+    private var elseScopeInContext: Boolean = false
+    private var thenScopeInContext: Boolean = false
 
+    fun setThenBlock(thenBlocks:MutableList<InstructionBlock>) {
+        this.thenBlock = thenBlocks
+    }
+    fun setElseBlock(elseBlocks: MutableList<InstructionBlock>) {
+        this.elseBlock = elseBlocks
+    }
 
+    fun addElseScopeInContext() {
+        if (thenScopeInContext) {
+            context.popScope()
+        }
+        if (elseScopeInContext) return
+        context.pushScope(elseScope)
+        elseScopeInContext = true
+    }
+    fun addThenScopeInContext() {
+        if (elseScopeInContext) {
+            context.popScope()
+        }
+        if (thenScopeInContext) return
+        context.pushScope(thenScope)
+        thenScopeInContext = true
+    }
 
-
+    /**
+     * Обрабатывает входные данные условия, проверяет оператор сравнения и выполняет сравнение.
+     * Удаляет scope из context по
+     * @param leftPartCondition Левая часть условия (например, строка или значение для сравнения).
+     * @param rightPartCondition Правая часть условия (например, строка или значение для сравнения).
+     * @param operator Оператор сравнения (например, ">", "<", "==", "!=", "<=", ">=").
+     * @param thenBlock Список блоков, которые будут выполнены при успешной проверке условия.
+     * @param elseBlock Список блоков, которые будут выполнены при успешной проверку условия.
+     *
+     * @return Код результата выполнения:
+     *   - [SUCCESS.id] — успешное выполнение.
+     *   - [NO_COMPARISON_OPERATOR_SELECTED.id] — не выбран оператор сравнения.
+     *   - Код ошибки из метода [compare], если сравнение частей условия завершилось с ошибкой.
+     */
     fun processInput(leftPartCondition: String, rightPartCondition: String, operator: String, thenBlock: MutableList<InstructionBlock>, elseBlock: MutableList<InstructionBlock>): Int {
         this.leftPartCondition = leftPartCondition
         this.rightPartCondition = rightPartCondition
@@ -29,22 +68,16 @@ class ConditionBlock(
         this.thenBlock = thenBlock
         this.elseBlock = elseBlock
 
-        val errorCompare =  compare()
+        if (this.operator == "Выбрать оператор") return NO_COMPARISON_OPERATOR_SELECTED.id
+
+        val errorCompare = compare()
+
         if(errorCompare != SUCCESS.id) {
-            rollbackThenBlock()
-            rollbackElseBlock()
+            context.popScope()
             return errorCompare
         }
 
-        if(resultValue) {
-            rollbackElseBlock()
-            rollThenBlock()
-        }
-        else {
-            rollbackThenBlock()
-            rollElseBlock()
-        }
-
+        context.popScope()
         return SUCCESS.id
     }
 
@@ -77,49 +110,12 @@ class ConditionBlock(
         return SUCCESS.id
     }
 
-    fun setThenBlock(thenBlocks:MutableList<InstructionBlock>) {
-        this.thenBlock = thenBlocks
-
-        return
-    }
-
-    fun setElseBlock(elseBlocks: MutableList<InstructionBlock>) {
-        this.elseBlock = elseBlocks
-
-        return
-    }
-
-    fun rollbackThenBlock() {
-        rollbackActions(thenBlock)
-
-        return
-    }
-
-    fun rollbackElseBlock() {
-        rollbackActions(elseBlock)
-
-        return
-    }
-
-    fun rollThenBlock() {
-        rollActions(thenBlock)
-
-        return
-    }
-
-    fun rollElseBlock() {
-        rollActions(elseBlock)
-
-        return
-    }
-
     override fun removeBlock() {
-        rollbackThenBlock()
-        rollbackElseBlock()
+        return
     }
 
     override fun run(): Int {
-        context.pushScope(scope)
+        context.pushScope(hashMapOf())
         val compareError = compare()
         if (compareError != 0){
             context.popScope();
