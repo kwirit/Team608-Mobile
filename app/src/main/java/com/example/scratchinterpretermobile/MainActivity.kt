20ee4e8b825dc7c2594d9250adb146d85b2f7ac4
Egor_Granin <epichescktdm@gmail.com>
2025-05-31 00:12:02 +0700

    added painted system padding

--- app/src/main/java/com/example/scratchinterpretermobile/MainActivity.kt
+++ app/src/main/java/com/example/scratchinterpretermobile/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.scratchinterpretermobile
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -13,6 +14,7 @@ import com.example.scratchinterpretermobile.View.ViewModels.MainViewModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
         val mainViewModel: MainViewModel by viewModels()
         setContent {
